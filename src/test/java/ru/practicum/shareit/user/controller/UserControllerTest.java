f210ffeeade0ffad8e42d354740ffbdb51354611
VldSab <vladimir.saber@gmail.com>
2023-05-10 22:41:34 +0300

    feat: Реализовать функционал работы с запросами предметов. Реализовать пагинацию у Booking и Item.  Добавить тесты ItemServiceStandard, UserServiceStandard, BookingServiceStandard, ItemRequestServiceStandard и репозиториев.

---
+++ src/test/java/ru/practicum/shareit/user/controller/UserControllerTest.java
@@ -0,0 +1,26 @@
+package ru.practicum.shareit.user.controller;
+
+import org.junit.jupiter.api.Test;
+
+class UserControllerTest {
+
+    @Test
+    void listUsers() {
+    }
+
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void addUser() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+}
